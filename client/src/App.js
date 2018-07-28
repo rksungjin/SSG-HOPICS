004680159ff67db0701a520ec6a557e06bb2c207
Jane Asawa <asawajane@gmail.com>
2018-07-28 14:31:25 -0700

    added App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GeneralIssues from "./GeneralPage/GeneralIssues";
+import Detail from "./GeneralPage/Detail";
+import NoMatch from "./GeneralPage/NoMatch";
+import Nav from "./components/Nav";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={GeneralIssues} />
+        <Route exact path="/generalissues" component={GeneralIssues} />
+        <Route exact path="/generalissues/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;

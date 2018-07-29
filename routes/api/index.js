153012383b68a35e0550515443d4a8fbb7514c28
Jane Asawa <asawajane@gmail.com>
2018-07-28 17:43:56 -0700

    changed to blog: seedDB, routes, models, blogController

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const bookRoutes = require("./blogs");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/blogs", blogRoutes);
 
 module.exports = router;
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
+// This file empties the Blogs collection and inserts the blogs below
 
 mongoose.connect(
   process.env.MONGODB_URI ||
@@ -123,9 +123,9 @@ const bookSeed = [
   }
 ];
 
-db.Book
+db.Blog
   .remove({})
-  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(() => db.Blog.collection.insertMany(blogSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);

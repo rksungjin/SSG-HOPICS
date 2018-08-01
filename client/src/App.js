736419701917a5961af44cd87df1df4ec8f9175f
Sam Torosyan <samtorosyan@gmail.com>
2018-07-31 19:31:42 -0700

    Update Nav

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import Blogs from "./blogPages/Blogs";
 import Detail from "./blogPages/Detail";
 import NoMatch from "./blogPages/NoMatch";
 import Nav from "./components/Nav";
+import lAAPI from "./lAAPI/"
+import newsletter from "./newsletter/Newsletter";
+
+
 
 const App = () => (
   <Router>
@@ -14,6 +18,8 @@ const App = () => (
         <Route exact path="/blogs" component={Blogs} />
         <Route exact path="/blogs/:id" component={Detail} />
         <Route component={NoMatch} />
+        <Route exact path="/lAAPI/" component ={lAAPI} />
+        <Route exact path="/newsletter/" component= {Newsletter} />
       </Switch>
     </div>
   </Router>

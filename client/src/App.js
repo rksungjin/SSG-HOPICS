89157e2a6ab461f7e00c2c91cedc68b86af222b9
Sam Torosyan <samtorosyan@gmail.com>
2018-07-31 19:50:52 -0700

    navbar2

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Detail from "./blogPages/Detail";
 import NoMatch from "./blogPages/NoMatch";
 import Nav from "./components/Nav";
 import lAAPI from "./lAAPI/"
-import newsletter from "./newsletter/Newsletter";
+import Newsletter from "./newsletter/Newsletter";
 
 
 
@@ -19,7 +19,7 @@ const App = () => (
         <Route exact path="/blogs/:id" component={Detail} />
         <Route component={NoMatch} />
         <Route exact path="/lAAPI/" component ={lAAPI} />
-        <Route exact path="/newsletter/" component= {Newsletter} />
+        <Route exact path="/newsletter" component= {Newsletter} />
       </Switch>
     </div>
   </Router>
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -17,6 +17,7 @@ const Nav = () => (
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
+    
       <ul className="navbar-nav">
         <li className="nav-item">
           <Link className="nav-link" to="/">Home</Link>    
@@ -25,7 +26,7 @@ const Nav = () => (
           <Link className="nav-link" to="/api">API</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Announcements">Announcements</Link>
+          <Link className="nav-link" to="/Blogs">General</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/logout">Log Out</Link>

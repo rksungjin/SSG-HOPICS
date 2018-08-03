0bf328d0dd2a11d4c8da8717c715906436bc58c2
Richard Kim <rk.sungjin@gmail.com>
2018-08-02 19:49:29 -0700

    rebase

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,15 @@
 module.exports = function(app){
-	// Our model controllers (rather than routes)
-	const users = require('./routes/users');
-	const trips = require('./routes/trips');
-	const pricing = require('./routes/pricing');
-	const blog = require('./routes/blogs');
+		// Our model controllers (rather than routes)
+		const users = require('./routes/users');
+		const trips = require('./routes/trips');
+		const pricing = require('./routes/pricing');
 
-	const authCheckMiddleware = require('./config/middleware/authCheck');
-	app.use('/apis/trips', authCheckMiddleware);
-	app.use('/apis/pricing', authCheckMiddleware);
+		const authCheckMiddleware = require('./config/middleware/authCheck');
+		app.use('/apis/trips', authCheckMiddleware);
+		app.use('/apis/pricing', authCheckMiddleware);
 
-	app.use('/apis/users', users);
-	app.use('/apis/trips', trips);
-	app.use('/apis/pricing', pricing);
-	app.use('/apis/blog', blog);
-	//other routes..
+		app.use('/apis/users', users);
+		app.use('/apis/trips', trips);
+		app.use('/apis/pricing', pricing);
+		//other routes..
 }

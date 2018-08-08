c510e364b1e494228304422014fee1c39e42baea
Jane Asawa <asawajane@gmail.com>
2018-08-08 13:16:26 -0700

    added Announcement.css to pull content of page below Nav background

--- app/components/Announcement/Announcement.js
+++ app/components/Announcement/Announcement.js
@@ -1,6 +1,7 @@
 // Include React
 import React, { Component } from 'react';
 import Nav from '../children/Nav'
+import './Announcement.css';
 
 // Creating the Main component
 export default class Announcement extends Component {
@@ -17,9 +18,9 @@ export default class Announcement extends Component {
        
 
 
-            <header className="mt-5">
+            <header className="Announcement">
                 <div className="">
-                    <div className="container mt-5">
+                    <div className="container">
                         <div className="row">
                             <div className="col-md-8 col-md-offset-2">
                                 <h1> testing of Announcement Page</h1>

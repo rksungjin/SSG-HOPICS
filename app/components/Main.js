c482879fdeee0033d30b831f9e13d139fa919b40
Sam Torosyan <samtorosyan@gmail.com>
2018-08-07 21:26:37 -0700

    main page

--- app/components/Main.js
+++ app/components/Main.js
@@ -45,14 +45,16 @@ export default class Main extends Component {
                 <div className="row">
                     <div className="col-lg">
                     
-                    <h1>Our Mission</h1>                  
+                    <h1><b>Our Mission</b></h1>                  
                     <p>HOPICS is dedicated to providing the highest quality innovative social service to South Los Angeles with an emphasis on behavioral health and housing stability.</p>
-                 <div className="col-lg">   
-                    <h1>Our Vision</h1>                    
+                 <div className="col-lg"> 
+                 
+                    <h1><b>Our Vision</b></h1>                    
                     <p>To provide the best quality of service to homeless and low-income households in South Los Angeles and surrounding areas and to employ the most qualified staff availableand build our services and programs to meet the diverse needs of the community.</p>
                 </div>
                 </div>
                 </div>
+                
             </div>
         </section>
 

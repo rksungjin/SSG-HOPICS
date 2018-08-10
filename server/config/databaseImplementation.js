330f675fa342f91067277341d20e5031a4f39253
Richard Kim <rk.sungjin@gmail.com>
2018-08-10 12:37:46 -0700

    mongodb connection

--- server/config/databaseImplementation.js
+++ server/config/databaseImplementation.js
@@ -1,9 +1,11 @@
 const mongoose 			 = require('mongoose');
-const configDB = require('./databaseCreds');
-mongoose.connect(configDB.url);
+// const configDB = require('./databaseCreds');
+// mongoose.connect(configDB.url);
 
 //Get the default connection
-const db = mongoose.connection;
+mongoose.connect("mongodb://richardkim:b3wh910w@ds219432.mlab.com:19432/heroku_64jfqms9");
+//<dbuser>:<dbpassword>@ds219432.mlab.com:19432/heroku_64jfqms9
+// const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
